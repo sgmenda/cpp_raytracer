c40d4692f78f2498cc11d208639a0f8a5af83b38
Sanketh Menda <mendasanketh@gmail.com>
2019-01-19 19:44:51 -0500

    finished chapter 7

--- main.cpp
+++ main.cpp
@@ -4,13 +4,23 @@
 #include "ray.h"
 #include "camera.h"
 
+// Generates a random vector in the unit sphere
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 // Linearly blends white and blue.
 // t=1.0 is blue and t=0.0 is white
 // and has a sphere
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5*color( ray(rec.p, target-rec.p), world );
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
@@ -46,6 +56,7 @@ int main(){
             }
 
             col /= float(ns);
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) ); // Gamma correction
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
