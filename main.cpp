8877f9a5a51052bfc96bd4c35093da50d6628967
Sanketh Menda <mendasanketh@gmail.com>
2019-01-19 19:33:01 -0500

    finished chapter 6

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "ray.h"
+#include "camera.h"
 
 // Linearly blends white and blue.
 // t=1.0 is blue and t=0.0 is white
@@ -20,30 +21,31 @@ vec3 color(const ray& r, hitable *world) {
 int main(){
     int nx = 200;
     int ny = 100;
+    int ns = 100;
 
     // Boilerplate for PPM file
     std::cout << "P3" << std::endl;             // P3 means ASCII
     std::cout << nx << " " << ny << std::endl;  // cols, rows
     std::cout << "255" << std::endl;            // Max color
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-
     hitable *list[2];
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0,-100.5,-1), 100);
     hitable *world = new hitable_list(list, 2);
+    camera cam; 
 
     for (int j = ny-1; j >= 0; j--){
         for (int i = 0; i < nx; i++){
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s ++) {
+                float u = float(i) / float(nx);
+                float v = float(j) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
 
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
